3b1a26dfecebd99648a64ba8d65d55b8a7f04024
Guilherme Henrique Amaral <101069377+ItalianFanBoy@users.noreply.github.com>
2022-06-11 14:19:38 -0300

    Estudando estruturas EJS

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ app.set('view engine', 'ejs');
 app.get("/:nome/:lang", (req, res) =>{
     var nome = req.params.nome;
     var lang = req.params.lang;
+    var exibirMsg = true;
    res.render("index",{
        nome: nome,
        lang: lang,
        empresa: "Guia do programador",
        inscritos: 8000,
+       msg: exibirMsg,
    });
 });
 
