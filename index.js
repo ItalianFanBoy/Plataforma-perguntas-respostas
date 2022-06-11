4d433ee6cdae898b2571eb835aefb8bff73124e7
Guilherme Henrique Amaral <101069377+ItalianFanBoy@users.noreply.github.com>
2022-06-11 17:13:08 -0300

    Começando projeto
    Instalação do bootstrap e confi do ejs

--- index.js
+++ index.js
@@ -3,26 +3,10 @@ const app = express();
 
 //estou dizendo para o Express usar o EJS como View engine
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-app.get("/:nome/:lang", (req, res) =>{
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = true;
-    var produtos =[
-        {nome: "Doritos", preco: 3.14},
-        {nome: "coca-cola", preco:5},
-        {nome: "Leite", preco:1.45},
-        {nome: "Carne", preco: 20},
-        {nome: "RedBull", preco:12},
-];
-   res.render("index",{
-       nome: nome,
-       lang: lang,
-       empresa: "Guia do programador",
-       inscritos: 8000,
-       msg: exibirMsg,
-       produtos: produtos,
-   });
+app.get("/", (req, res) =>{
+    res.render("index",);
 });
 
 app.listen(8080, ()=>{
