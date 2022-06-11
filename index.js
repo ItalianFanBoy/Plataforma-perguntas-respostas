13b3bdca7395f5a5e12d859c8dcc1c3b1022fc90
Guilherme Henrique Amaral <101069377+ItalianFanBoy@users.noreply.github.com>
2022-06-11 16:40:36 -0300

    ejs

--- index.js
+++ index.js
@@ -8,12 +8,20 @@ app.get("/:nome/:lang", (req, res) =>{
     var nome = req.params.nome;
     var lang = req.params.lang;
     var exibirMsg = true;
+    var produtos =[
+        {nome: "Doritos", preco: 3.14},
+        {nome: "coca-cola", preco:5},
+        {nome: "Leite", preco:1.45},
+        {nome: "Carne", preco: 20},
+        {nome: "RedBull", preco:12},
+];
    res.render("index",{
        nome: nome,
        lang: lang,
        empresa: "Guia do programador",
        inscritos: 8000,
        msg: exibirMsg,
+       produtos: produtos,
    });
 });
 
