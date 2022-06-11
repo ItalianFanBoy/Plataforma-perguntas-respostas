9690c8b869afac33af9300c55e6667ed2bde24ae
Guilherme Henrique Amaral <101069377+ItalianFanBoy@users.noreply.github.com>
2022-06-11 14:00:31 -0300

    Trabalhando com EJS
    EJS e requisições

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const app = express();
 //estou dizendo para o Express usar o EJS como View engine
 app.set('view engine', 'ejs');
 
- 
-
-app.get("/", (req, res) =>{
-   res.render("home");
+app.get("/:nome/:lang", (req, res) =>{
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+   res.render("index",{
+       nome: nome,
+       lang: lang,
+       empresa: "Guia do programador",
+       inscritos: 8000,
+   });
 });
 
 app.listen(8080, ()=>{
