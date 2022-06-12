cfb4e793c4aad8baee1a086352db5ef2d972b918
Guilherme Henrique Amaral <101069377+ItalianFanBoy@users.noreply.github.com>
2022-06-11 21:10:16 -0300

    falta correção
    recentemente descobri que o parser nao recebe os dados tenho que usar o mult necessária atualização

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 //estou dizendo para o Express usar o EJS como View engine
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+//bodyparser tradutor de dados para captura no backend
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) =>{
     res.render("index",);
@@ -13,6 +18,12 @@ app.get("/perguntar", (req, res) =>{
     res.render("perguntar")
 })
 
+app.post("/salvarpergunta",(req, res) =>{
+    var titulo = req.body.titulo;
+    var descrição = req.body.descricao;
+    res.send("Formulário recebido! titulo" + " " + titulo + " " + "descrição " + descrição);
+});
+
 app.listen(8080, ()=>{
     console.log("App rodando!");
-});
+});  
