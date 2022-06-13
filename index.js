dec231bf3563b997dbb1297422c8196bddfabb92
Guilherme Henrique Amaral <101069377+ItalianFanBoy@users.noreply.github.com>
2022-06-12 23:11:34 -0300

    busca condicional

--- index.js
+++ index.js
@@ -47,6 +47,20 @@ app.post("/salvarpergunta",(req, res) =>{
     })
 });
 
+
+app.get("/pergunta/:id",(req, res) =>{
+    var id = req.params.id;
+    Pergunta.findOne({
+        where:{id: id}
+    }).then(pergunta =>{
+        if(pergunta != undefined){
+            res.render("pergunta");
+        }else{
+            res.redirect("/")
+        }
+    })
+});
+
 app.listen(8080, ()=>{
     console.log("App rodando!");
 });  
